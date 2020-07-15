c54af4ef4ba03c358923de2e9e46507d1b6001bf
julian <estrotococoa@gmail.com>
2020-07-15 15:57:53 -0300

    tried to fix the unshowed icons

--- PortfolioProject/src/app/app.module.ts
+++ PortfolioProject/src/app/app.module.ts
@@ -1,9 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faGithub, faCodepen, faInstagram, faYoutube, faLinkedin, faMedium } from '@fortawesome/free-brands-svg-icons';
+
+library.add(faGithub, faCodepen, faInstagram, faYoutube, faLinkedin, faMedium);
 
 @NgModule({
   declarations: [
@@ -11,9 +18,11 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
