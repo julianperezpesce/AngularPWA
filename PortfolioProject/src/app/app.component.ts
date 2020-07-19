9890523c7fd8e227f8d87742ace688dbe10499e0
julian <estrotococoa@gmail.com>
2020-07-19 15:16:53 -0300

    new component & colored html sections

--- PortfolioProject/src/app/app.component.ts
+++ PortfolioProject/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// import { faGithub, faCodepen, faInstagram, faYoutube, faLinkedin, faMedium } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PortfolioProject';
+  // faGithub = faGithub;
+  // faCodepen = faCodepen;
+  // faLinkedin = faLinkedin;
+  // faInstagram = faInstagram;
+  // faYoutube = faYoutube;
+  // faMedium = faMedium;
 }
--- PortfolioProject/src/app/app.module.ts
+++ PortfolioProject/src/app/app.module.ts
@@ -6,15 +6,17 @@ import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faGithub, faCodepen, faInstagram, faYoutube, faLinkedin, faMedium } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faCodepen, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
 
-library.add(faGithub, faCodepen, faInstagram, faYoutube, faLinkedin, faMedium);
+// library.add(faGithub, faCodepen, faLinkedin);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent
   ],
   imports: [
     BrowserModule,
@@ -25,4 +27,10 @@ library.add(faGithub, faCodepen, faInstagram, faYoutube, faLinkedin, faMedium);
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private library2: FaIconLibrary) {
+    library2.addIcons(faCodepen, faGithub, faLinkedin);
+  }
+}
+
+
